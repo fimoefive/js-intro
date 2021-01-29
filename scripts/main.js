a17f1ae4b19a44607601dd75341f33b46dde7f23
fimoefive <brothermj@hotmail.com>
2021-01-28 18:16:27 -0600

    formating changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,55 @@
 console.log("JavaScript");
 
+// ********** START VARIABLES *************
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+firstName = 'Dr. T';
+lastName = 'Vasquez';
+
+// console.log(firstName + lastName);
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`);
+
+// const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Gumbo';
+// console.log(favoriteFood);
+
+// ******** END VARIABLES **********
+
+// ******** START DATA TYPES **********
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// ******** END DATA TYPES **********
+
+// ******** START MATH **********
+// console.log(Math.pow(2, 2)); // 4
+// console.log(Math.pow(3, 2)); // 9
+// console.log(Math.pow(3, 3)); // 27
+
+// console.log(Math.round(6.5)); // 7
+// console.log(Math.round(6.45)); // 6
+// console.log(Math.floor(6.9999)); // 6
+// console.log(Math.ceil(6.0000001)); // 7
+
+const cat = 'kitty';
+console.log(cat.length); // 5
+
+// ******** END MATH **********
+
 let firstName = 'Martin';
 let lastName = 'Sisk';
 
@@ -74,3 +124,73 @@ const canDrive = (age) => {
 const ans = canDrive(13);
 console.log(ans);
 console.log(canDrive(15));
+
+// ******* FUNCTIONS ********
+
+// console.log("CONNECTED");
+
+// function sayHello() {
+//   console.log("Hello!");
+// }
+
+// const sayHello = function() {
+//   console.log("Hello!");
+// }
+
+// const sayHello = () => {
+//   console.log("Hello!");
+// }
+
+// sayHello();
+
+// ******* FUNCTIONS WITH ARGUMENTS *****
+function logsHello(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// logsHello('Dr. T');
+const sum = 55;
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log("Console log inside the function", sum);
+  return sum;
+}
+
+console.log(sum);
+console.log(addsTwoNumbers(2, 1));
+console.log(sum);
+
+
+// ********* Control Flow *********
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1 !== 'nice' && arg2 !== 'Nice') {
+//     console.log(`You are ${arg1}`);
+//   } else {
+//     console.log('false');
+//   }
+// }
+
+// function thisIsTrue(arg1, arg2) {
+//   if (arg1.length < 3) {
+//     console.log(`You are ${arg1}`);
+//   } else if (arg2.length > 8) {
+//     console.log(`You are ${arg2}`);
+//   } else {
+//     console.log('You are NOTHING!');
+//   }
+// }
+
+// thisIsTrue('ni', 'mickeymouse');
+
+const canDrive = (age) => {
+  if (age >= 15) {
+    return true;
+  }
+
+  return false;
+}
+
+const answer = canDrive(12);
+console.log(answer);
